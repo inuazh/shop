9612c2a0fde404a0d4c9f868577479e3321d0232
inuazh <YOUR_EMAIL>
2023-05-31 13:41:31 +0300

    imgSSSS(yummy)

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <div>
+            <span className="logo">Sweetshop</span>
+            <ul className='nav'>
+                <li>About us</li>
+                <li>Contacts</li>
+            </ul>
+        </div>
+        <div className='presentation'></div>
+    </header>
+  )
+}
